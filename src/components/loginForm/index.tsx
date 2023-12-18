08004b48339ae37c23b2b469de1f4447b30aae36
Maxim Arzhanov <arzhanovmaxim@gmail.com>
2023-12-18 16:01:56 +0600

    feat: install formik and configure state of auth form

--- src/components/loginForm/index.tsx
+++ src/components/loginForm/index.tsx
@@ -1,16 +1,44 @@
 import { Button, Checkbox, Label, TextInput } from 'flowbite-react';
 import { Link } from 'react-router-dom';
+import { useFormik } from 'formik';
+import { ILoginFormValues } from './types';
 
 function LoginForm() {
+  const initialValues: ILoginFormValues = {
+    email: '',
+    password: '',
+  };
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <div className='container max-w-xs rounded-xl shadow-lg mx-4 pb-8'>
       <h1 className='py-6 bg-gradient-to-r from-indigo-700 bg-fuchsia-500 text-white text-2xl font-bold text-center rounded-t-xl'>
         Login Form
       </h1>
 
-      <form className='m-5 flex flex-col gap-4'>
-        <TextInput name='email' type='email' placeholder='Email Address' style={{ borderRadius: '24px' }} />
-        <TextInput name='password' type='password' placeholder='Password' style={{ borderRadius: '24px' }} />
+      <form className='m-5 flex flex-col gap-4' onSubmit={formik.handleSubmit}>
+        <TextInput
+          name='email'
+          type='email'
+          placeholder='Email Address'
+          style={{ borderRadius: '24px' }}
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        <TextInput
+          name='password'
+          type='password'
+          placeholder='Password'
+          style={{ borderRadius: '24px' }}
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        />
 
         <div className='flex justify-between px-2 flex-wrap gap-1'>
           <div className='flex items-center gap-1 text-sm'>
